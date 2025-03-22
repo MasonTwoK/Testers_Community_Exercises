b4ecbd190e54fc2060f62680037ca414a06eca1e
willhamilton <willhamilton1991@gmail.com>
2025-03-22 03:20:06 +0200

    Update hometask_07 after feedback

--- hometasks/Stage_1/hometask_07.py
+++ hometasks/Stage_1/hometask_07.py
@@ -5,23 +5,20 @@ while i < 10:
     i += 1
 
 # Задача 2 Сума чисел: Напишіть програму, яка використовує цикл while для підрахунку суми чисел від 1 до 100.
-print("App that provides Triangular number")
-entered_number = int(input("Enter your number: "))
-
+print("App that provides Triangular number of 100")
 iterator = 1
 nums_sum = 0
 
-while iterator <= entered_number:
+while iterator <= 100:
     nums_sum += iterator
     iterator += 1
 
-    print(f"Triangle numbers of {entered_number} is {nums_sum}")
+print(f"Triangle numbers of 100 is {nums_sum}")
 
 # Задача 3 Зворотній відлік:
 # Напишіть програму, яка використовує цикл while для виведення зворотного відліку від 10 до 1,
 # після чого виводить "Старт!"
 countdown_num = 10
-
 while countdown_num > 0:
     print(countdown_num)
     countdown_num -= 1
