b251b0630d7ff17a8bbf31a1c8344561436835ce
willhamilton <willhamilton1991@gmail.com>
2025-03-23 20:35:07 +0200

    Update hometask_15.py after feedback

--- hometasks/Stage_2/hometask_15.py
+++ hometasks/Stage_2/hometask_15.py
@@ -1,33 +1,28 @@
 # Задача 1: Напишіть програму, що створює словник для конвертації валют,
 # де ключами є валютні коди (наприклад, USD, EUR), а значеннями - курс обміну до гривні.
-currencies = ['USD', 'EUR', 'GBP']
-list_of_pairs = []
-
-for currency in currencies:
-    rate = input(f"Enter rate for {currency} currency: ")
-    exchange_pair = [currency, rate]
-    list_of_pairs.append(exchange_pair)
-
-currencies_exchange = dict(list_of_pairs)
-
-
-# Задача 2: Уявімо, що ми маємо список предметів, і нам потрібно створити словник, де кожен предмет буде ключем,
-# а початкове значення для кожного ключа буде встановлене як певна оцінка (наприклад, 0).
-list_of_items = ['spoon', 'fork', 'knife']
-dict_of_items = dict.fromkeys(list_of_items, 0)
+# currencies_exchange = {
+#     'USD': input(f"Enter rate for 'USD' currency: "),
+#     'EUR': input(f"Enter rate for 'EUR' currency: ")
+# }
+#
+# # Задача 2: Уявімо, що ми маємо список предметів, і нам потрібно створити словник, де кожен предмет буде ключем,
+# # а початкове значення для кожного ключа буде встановлене як певна оцінка (наприклад, 0).
+# list_of_items = ['spoon', 'fork', 'knife']
+# dict_of_items = dict.fromkeys(list_of_items, 0)
 
 # Задача 3: Напишіть програму для створення словника, де ключами будуть імена студентів, а значеннями - їхні середні бали
 
 # Why developers left defined values at the top of method/function?
 students_list = []
-sum_of_student_marks = 0
 
-student_switch = True
-while student_switch:
+main_logic_switch = True
+while main_logic_switch:
     student_name = input("Enter student name: ")
     student_marks = input("Enter: student marks: ")
 
     list_of_student_marks = student_marks.split(' ')
+
+    sum_of_student_marks = 0
     for mark in list_of_student_marks:
         sum_of_student_marks += int(mark)
 
@@ -35,15 +30,15 @@ while student_switch:
     students_list.append([student_name, avg_student_mark])
 
     print("Do you want add more students?")
-    additional_student_switch = True
-    while additional_student_switch:
-        decision = input("Enter 1. - Yes, 0. - No.. Enter your choice: ")
-        if decision == '1':
-            student_switch = True
-            additional_student_switch = False
-        if decision == '0':
-            student_switch = False
-            additional_student_switch = False
+    add_student_answer_switch = True
+    while add_student_answer_switch:
+        answer = input("Enter 1. - Yes, 0. - No.. Enter your choice: ")
+        if answer == '1':
+            main_logic_switch = True
+            add_student_answer_switch = False
+        if answer == '0':
+            main_logic_switch = False
+            add_student_answer_switch = False
         else: print("Incorrect input. Try again..")
 
 dict_of_students = dict(students_list)
