9e454c056c87071049d0f02b3e9bf13042366fb4
willhamilton <willhamilton1991@gmail.com>
2025-03-31 22:41:21 +0300

    Update added lecture hometask_17.py

---
+++ lessons/Stage_2/lesson_17.py
@@ -0,0 +1,35 @@
+def example_1(number):
+    if number > 0:
+        print('positive number')
+    else:
+        if number < 0:
+            print('negative number')
+        else:
+            print('zero')
+
+
+def example_2(number):
+    if number > 0:
+        print('positive number')
+    elif number < 0:
+        print('negative number')
+    else:
+        print('number is zero')
+
+
+def example_3(number):
+    if number > 0:
+        print('positive number')
+    if number < 0:
+        print('negative number')
+    if number == 0:
+        print('number is zero')
+
+
+def example_4(number):
+    if number > 0:
+        print('positive number')
+    if number < 0:
+        print('negative number')
+    else:
+        print('zero')
