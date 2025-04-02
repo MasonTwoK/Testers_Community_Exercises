d8352e79ffa148406b079cd71620e1eaef665f58
willhamilton <willhamilton1991@gmail.com>
2025-04-02 23:14:35 +0300

    Added finished lesson_21.py

--- lessons/Stage_3/lesson_21.py
+++ lessons/Stage_3/lesson_21.py
@@ -1,18 +1,36 @@
+class Example1:
+    def foo(self, a):
+        return a ** 3
+
+    lambda_f1 = lambda x: x ** 3
 
 
-class MyFirstClass:
-    def say_hello(self):
-        print('Hello')
+class Example2:
+    def foo_2(self, number):
+        if number % 2 == 1:
+            return 'NO'
+        else:
+            return 'YES'
+
+    lambda_f2 = lambda number: 'no' if number % 2 == 1 else 'yes'  # I do not see logic in expression
 
 
-my_first_class = MyFirstClass()
-my_first_class.say_hello()
+def example_3():
+    list_a = [199, 2, 34, 909, -100, 18]
+    print(list_a)
 
-str_1 = str('Hi')
-print(type(str_1))
-print(str_1.lower())
+    list_a.sort(key=lambda x: x % 10)  # I do not understand who 'key=' works in this case
+    print(list_a)
 
-print('#' * 30)
-print(dir(my_first_class))
-print('#' * 30)
-print(dir(str_1))
+
+def example_4():
+    calculator = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b
+    }
+
+    result = calculator['+'](1, 2)  # I do not think that was in lesson 15 & 16
+    print(result)
+
