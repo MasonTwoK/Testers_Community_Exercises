a2579ef4af120f29ff518bc25a64f32207ff7716
willhamilton <willhamilton1991@gmail.com>
2025-04-04 02:23:56 +0300

    Updated hometask_8.py task 2 (rework)

--- hometasks/Stage_2/hometask_08.py
+++ hometasks/Stage_2/hometask_08.py
@@ -1,18 +1,48 @@
 # Задача 1: Напишіть програму, яка приймає рядок від користувача і виводить його у верхньому регістрі.
-def task_2():
-    entered_string = input("Enter you string: ")
-    result = entered_string.upper()
-    print(result)
+entered_string = input("Enter you string: ")
+result = entered_string.upper()
+print(result)
+
 
 # Задача 2: Створіть програму, яка приймає рядок від користувача та заміняє всі входження символу 'a' на '*',
 # якщо символ ‘а’ відсутній до відповідне повідомлення.
 entered_string = input("Enter the string: ")
-if entered_string.find('a') == -1:
-    print(" 'a' character is missing in entered string")
-else:
-    print(entered_string.replace('a', '*'))
 
-# Задача 3: Напишіть програму, яка приймає рядок від користувача та виводить його з позначенням кількості символів у ньому.
+list_of_letters = []
+for letter in entered_string:
+    list_of_letters.append(letter)
+
+base_list_of_letters = list_of_letters.copy()
+
+counter = 0
+for letter in list_of_letters:
+    if not letter.find('a') == -1:
+        list_of_letters[counter] = letter.replace('a', '*')
+        counter += 1
+        continue
+    if not letter.find('A') == -1:
+        list_of_letters[counter] = letter.replace('A', '*')
+        counter += 1
+        continue
+    if not letter.find('а') == -1:
+        list_of_letters[counter] = letter.replace('а', '*')
+        counter += 1
+        continue
+    if not letter.find('А') == -1:
+        list_of_letters[counter] = letter.replace('А', '*')
+        counter += 1
+        continue
+    counter += 1
+
+if list_of_letters == base_list_of_letters:
+    print("Characters 'A' and 'a' are missing in entered string")
+else:
+    result = ''.join(list_of_letters)
+    print(result)
+
+
+# Задача 3: Напишіть програму,
+# яка приймає рядок від користувача та виводить його з позначенням кількості символів у ньому.
 entered_string = input("Enter string: ")
 print(f"Entered string: {entered_string}")
 
