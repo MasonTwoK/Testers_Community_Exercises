aa37106bf28f7e7a5a66afd11765f3124e348f39
willhamilton <willhamilton1991@gmail.com>
2025-04-04 16:59:50 +0300

    Added hometask_5.py task 4

--- hometasks/Stage_1/hometask_05.py
+++ hometasks/Stage_1/hometask_05.py
@@ -2,33 +2,59 @@
 # Користувач дає вам список (тобто в програмі у вас буде змінна зі списком оцінок), наприклад:
 # class_marks = [11, 10, 11, 10, 8]
 # Вам потрібно вирахувати середню оцінку в групі.
-class_marks = [11, 10, 11, 10, 8]
 
-avg_mark = sum(class_marks) / len(class_marks)
-print(f"Average mark of group is: {avg_mark}")
+# class_marks = [11, 10, 11, 10, 8]
+#
+# avg_mark = sum(class_marks) / len(class_marks)
+# print(f"Average mark of group is: {avg_mark}")
 
 
 # Задача 2:
 # Попросіть користувача ввести рядок з 4 словами, збережіть це у список, видаліть два останні елементи.
 # І додайте в кінець один елемент з вашим іменем.
-words_string = input("Enter 4 random names: ")
-words_list = words_string.split(" ")
-words_list.pop()
-print("Pop called() Removed last name in list")
-words_list.pop()
-print("Pop called() Removed last name in list")
 
-words_list.append("Will")
-print("Added new name to list")
-
-print(words_list)
+# words_string = input("Enter 4 random names: ")
+# words_list = words_string.split(" ")
+# words_list.pop()
+# print("Pop called() Removed last name in list")
+# words_list.pop()
+# print("Pop called() Removed last name in list")
+#
+# words_list.append("Will")
+# print("Added new name to list")
+#
+# print(words_list)
 
 
 # Задача 3:
 # Попросіть користувача ввести рядок зі словами, підрахуйте кількість цих слів і виведіть користувачу,
 # скільки слів він ввів.
-words_string = input("Enter words: ")
-words_list = words_string.split(" ")
 
-result = len(words_list)
-print(f"Amount of entered words: {result}")
+# words_string = input("Enter words: ")
+# words_list = words_string.split(" ")
+#
+# result = len(words_list)
+# print(f"Amount of entered words: {result}")
+
+# **Додаткові завдання:**
+
+# Задача 4: Після того як ви допомогли Марині з додатком для її школи вона розповіла про це своїм знайомим.
+# І тепер До вас Звернувся Пан Микола який також є вчителем, але уже в місті Дніпро.
+# Він питає чи можете ви написати програму Яка буде формувати та зберігати список з присутніх студентів,
+# виводити його на екран, а також загальну кількість учнів в цьому списку?
+students = input("Введіть фамілії студентів через пробіл\n")
+list_of_students = students.split(' ')
+print(list_of_students)
+print(len(list_of_students))
+
+
+# Задача 5: Завдяки вашій допомозі друзям, про вас вже знають майже у всіх містах України.
+# Цього разу до вас звернувся Тарас з столиці в якого є невеликий супермаркет і він просить у вас допомогу.
+# Йому потрібна програма яка:
+# * Запитує в користувача вартість покупок.
+# * Запитує чи є купон на знижку. Купон може бути на відсоток, а може бути на суму в гривнях.
+# * Тому перепитуєте який саме купон є в покупця.
+# * Якщо це купон на відсоток то запитуєте який саме відсоток і віднімаєте його від загальної суми чеку.
+# * Якщо це купон на суму то також віднімаєте її від суми чеку.
+# * Після цього дякуєте користувачу і повертаєте йому кінцеву суму."
+def tas
