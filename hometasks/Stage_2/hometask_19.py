fe1bf5ba6098e90b58672c66ced9de04301d9d9d
willhamilton <willhamilton1991@gmail.com>
2025-04-04 16:42:13 +0300

    Updated hometask_19.py task 3

--- hometasks/Stage_2/hometask_19.py
+++ hometasks/Stage_2/hometask_19.py
@@ -1,4 +1,7 @@
 # Задача 1: Напишіть функцію sub_numbers, яка приймає два аргументи num1 та num2 і повертає їхню різницю.
+from hometasks.Stage_1.hometask_01 import result
+
+
 def sub_numbers(num_1, num_2):
     result = num_1 - num_2
     return result
@@ -16,9 +19,6 @@ def check_even_odd(number):
 # Задача 3: Напишіть функцію find_max, яка приймає три числа num1, num2, та num3 і повертає найбільше з них.
 def find_max(num_1, num_2, num_3):
     list_of_nums = [num_1, num_2, num_3]
-    list_of_nums.sort(reverse=True)
-    max_num = list_of_nums[0]
+    max_num = max(list_of_nums)
 
     return max_num
-
-
