00cc9db6e62c89ede336c71a76f7aa00a15e8e44
willhamilton <willhamilton1991@gmail.com>
2025-04-05 18:38:28 +0300

    Updated discriptions for hometask_10.py

--- hometasks/Stage_2/main.py
+++ hometasks/Stage_2/main.py
@@ -1,7 +1,9 @@
 from file_01 import addition, division
 from file_02 import rectangle_area
+from hometask_20 import weight_converter_kg_to_lb, weight_converter_lb_to_kg
 
 addition(num_1=10, num_2=2)
 division(num_1=10, num_2=2)
 rectangle_area(side_a=4, side_b=6)
-
+weight_converter_kg_to_lb(42)
+weight_converter_lb_to_kg(89)
