552cecdf902e29254a670b0ff129f122274737aa
willhamilton <willhamilton1991@gmail.com>
2025-04-06 17:36:02 +0300

    Updated hometask_17 removed unnecessary return

--- hometasks/Stage_2/hometask_10.py
+++ hometasks/Stage_2/hometask_10.py
@@ -19,9 +19,27 @@ print(f"Minimal value of tuple is {min(tuple_of_numbers)}")
 
 
 # **Додаткові завдання:**
-
-# Задача 4
+# Задача 4:
 # Попросіть в користувача ввести два кортежа де будуть тільки слова, збережіть їх у зміні.
 # Утворюють використовуючи ці дві зміні третю зміну яка буде містити два попередніх кортежа.
 # Тепер розділіть цих два кортежи на перший в якому будуть тільки слова до п'яти букв включно
 # та другий де будуть слова більше п'яти букв.
+list_result_1 = []
+list_result_2 = []
+
+words_1 = input("Введіть перший кортеж: ")
+tuple_1 = tuple(words_1.split(' '))
+
+words_2 = input("Введіть другий кортеж: ")
+tuple_2 = tuple(words_2.split(' '))
+
+total_tuple = tuple_1 + tuple_2
+
+for word in total_tuple:
+    if len(word) <= 5:
+        list_result_1.append(word)
+    else:
+        list_result_2.append(word)
+
+result_1 = tuple(list_result_1)
+result_2 = tuple(list_result_2)
